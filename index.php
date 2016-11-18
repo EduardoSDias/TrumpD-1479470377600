7f0f12ec3b5b6cf7ae9884803389d65c4d3d79d9
EduardoSDias <eduardo.silva.dias@hotmail.com>
2016-11-18 10:14:23 -0200

    Update index.php

--- index.php
+++ index.php
@@ -14,6 +14,7 @@
 			<td>
 				<h1 id = "message"><?php echo "Hello World!"; ?></h1>
 				<p class='description'></p> Thanks for creating a <span class="blue">PHP Starter Application</span>.
+				<a href="#">Link</a>
 			</td>
 		</tr>
 	</table>
